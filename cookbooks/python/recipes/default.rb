c436c61bc529a02d4481182281b81e2fb6e92f93
Hans-Gunther Schmidt <hans@otype.de>
2012-10-19 01:51:28 +0200

    fixing necessary dependencies

--- cookbooks/python/recipes/default.rb
+++ cookbooks/python/recipes/default.rb
@@ -18,6 +18,10 @@
 # limitations under the License.
 #
 
+include_recipe "build-essential"
+
+package "bzip2"
+
 include_recipe "python::#{node['python']['install_method']}"
 include_recipe "python::pip"
 include_recipe "python::virtualenv"
