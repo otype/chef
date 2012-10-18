2f69eac79dba18d51e5beeaf8d26a414c8c7b036
Hans-Gunther Schmidt <hans@otype.de>
2012-10-18 14:48:04 +0200

    adding openssh server cookbook + adding a service_restarts recipe in base for restarting all services AT THE END of the base role steps

--- cookbooks/ssh/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "apitrary"
-maintainer_email "hgs@apitrary.com"
-license          "All rights reserved"
-description      "Installs/Configures ssh"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
