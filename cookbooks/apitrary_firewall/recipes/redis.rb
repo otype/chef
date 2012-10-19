34c0400b77306df3494893528380d4aee2e3f143
Hans-Gunther Schmidt <hans@otype.de>
2012-10-19 18:52:27 +0200

    adding a firewall set for redis

---
+++ cookbooks/apitrary_firewall/recipes/redis.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: apitrary_firewall
+# Recipe:: web
+#
+# Copyright 2012, apitrary
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "simple_iptables"
+
+# Allow HTTP/HTTPS
+simple_iptables_rule "system" do
+  rule [
+           "-p tcp --dport 6379",
+       ]
+  jump "ACCEPT"
+end
