c8101c9221acd8df57c37d67efc1bb6ca8ab919b
Hans-Gunther Schmidt <hans@otype.de>
2012-11-18 21:17:03 +0100

    adding bugtracking cookbook + firewall rule

---
+++ cookbooks/apitrary_firewall/recipes/youtrack.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: apitrary_firewall
+# Recipe:: supervisor
+#
+# Copyright 2012, apitrary
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "simple_iptables"
+
+# Allow youtrack
+simple_iptables_rule "system" do
+  rule "-p tcp --dport 8000"
+  jump "ACCEPT"
+end
