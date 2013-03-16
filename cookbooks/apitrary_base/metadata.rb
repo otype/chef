d7b836cd80efbbbde83e7d15968d65ed405fc6dd
Hans-Gunther Schmidt <hans@otype.de>
2013-03-16 03:30:34 +0100

    adding apitrary_base

---
+++ cookbooks/apitrary_base/metadata.rb
@@ -0,0 +1,7 @@
+name             'cookbooks.apitrary_base'
+maintainer       'apitrary'
+maintainer_email 'official@apitrary.com'
+license          'All rights reserved'
+description      'Installs/Configures cookbooks.apitrary_base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/apitrary_base/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: cookbooks.apitrary_base
+# Recipe:: default
+#
+# Copyright 2013, apitrary
+#
+# All rights reserved - Do Not Redistribute
+#
