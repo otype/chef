28065eb6c81e046a6e99e9eb0bed30d518b62dd7
Hans-Gunther Schmidt <hans@otype.de>
2013-03-16 02:46:18 +0100

    cleaning up again ... need to create a new repository for opsworks with all cookbooks in root directory of repo

--- apitrarybase/metadata.rb
+++
@@ -1,8 +0,0 @@
-name              "apitrarybase"
-maintainer       "apitrary"
-maintainer_email "hgs@apitrary.com"
-license          "All rights reserved"
-description      "Installs/Configures base"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
-depends         "user"
