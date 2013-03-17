7b5a1d320625f6b30933e7a959f3755b77bafa78
Hans-Gunther Schmidt <hans@otype.de>
2013-03-17 01:56:33 +0100

    adding ssh key cookbook

---
+++ apitrary_ssh_keys/metadata.rb
@@ -0,0 +1,7 @@
+name             'apitrary_ssh_keys'
+maintainer       'apitrary'
+maintainer_email 'official@apitrary.com'
+license          'All rights reserved'
+description      'Installs/Configures apitrary_ssh_keys'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
