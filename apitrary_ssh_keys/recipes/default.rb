55cf78c16d97544014e568d38ed19008db472b61
Hans-Gunther Schmidt <hans@otype.de>
2013-03-19 18:59:54 +0100

    removing all unnecessary cookbooks, creating a base cookbook for apitrary => move all required steps into fabric

--- apitrary_ssh_keys/recipes/default.rb
+++ apitrary_ssh_keys/recipes/default.rb
@@ -6,44 +6,47 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-directory "/root/.ssh" do
-  owner "root"
+directory "/home/ubuntu/.ssh" do
+  owner "ubuntu"
   mode 0755
   action :create
-  not_if {File.exists?("/root/.ssh")}
+  not_if {File.exists?("/home/ubuntu/.ssh")}
 end
 
-template "/root/.ssh/config" do
-  source "ssh_config.erb"
-  mode 0644
-  owner "root"
-  group "root"
-end
+# TODO: Fix this one or find another solution!
+# Following not working on OpsWorks nodes due to Chef overwriting this file for OpsWorks
+#template "/home/ubuntu/.ssh/config" do
+#  source "ssh_config.erb"
+#  mode 0644
+#  owner "ubuntu"
+#  group "ubuntu"
+#end
 
-template "/root/.ssh/live-bitbucket-ro" do
-  source "live-bitbucket-ro.erb"
-  mode 0600
-  owner "root"
-  group "root"
-end
+# TODO: Remove if unused
+#template "/home/ubuntu/.ssh/live-bitbucket-ro" do
+#  source "live-bitbucket-ro.erb"
+#  mode 0600
+#  owner "ubuntu"
+#  group "ubuntu"
+#end
+#
+#template "/home/ubuntu/.ssh/live-bitbucket-ro.pub" do
+#  source "live-bitbucket-ro.pub.erb"
+#  mode 0644
+#  owner "ubuntu"
+#  group "ubuntu"
+#end
 
-template "/root/.ssh/live-bitbucket-ro.pub" do
-  source "live-bitbucket-ro.pub.erb"
-  mode 0644
-  owner "root"
-  group "root"
-end
-
-template "/root/.ssh/apitrary-staging-deploy" do
+template "/home/ubuntu/.ssh/apitrary-staging-deploy" do
   source "apitrary-staging-deploy.erb"
   mode 0600
-  owner "root"
-  group "root"
+  owner "ubuntu"
+  group "ubuntu"
 end
 
-template "/root/.ssh/apitrary-staging-deploy.pub" do
+template "/home/ubuntu/.ssh/apitrary-staging-deploy.pub" do
   source "apitrary-staging-deploy.pub.erb"
   mode 0644
-  owner "root"
-  group "root"
+  owner "ubuntu"
+  group "ubuntu"
 end
