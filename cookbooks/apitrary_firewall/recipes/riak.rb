a51d11a6736714adb32aefe3b9a1a70c27b50f4c
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-04-02 14:09:05 +0200

    opening up port for Riak Control

--- cookbooks/apitrary_firewall/recipes/riak.rb
+++ cookbooks/apitrary_firewall/recipes/riak.rb
@@ -23,7 +23,7 @@ end
 # Get all riak server nodes IPs
 riak_nodes = search(:node, "chef_environment:#{node.chef_environment} AND role:riak-cluster")
 
-# Allow connections from app server nodes to Riak nodes
+# Allow connections from Riak nodes to Riak nodes
 riak_nodes.each do |riak_node|
   simple_iptables_rule "system" do
     rule "-p tcp -s #{riak_node['ipaddress']}"
@@ -44,4 +44,10 @@ loadbalancer_nodes.each do |lb_node|
     ]
     jump "ACCEPT"
   end
+end
+
+# Allow connections from everywhere to Riak Control Web Interface
+simple_iptables_rule "system" do
+  rule "-p tcp --dport 8069"
+  jump "ACCEPT"
 end
