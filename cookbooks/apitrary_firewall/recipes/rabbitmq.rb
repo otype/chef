d4b2c660be3da960b02c1fec35ede6440e28d5ba
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-04-04 23:49:41 +0200

    cleaning up + removing railsweb access (all moved to cloudControl)

--- cookbooks/apitrary_firewall/recipes/rabbitmq.rb
+++ cookbooks/apitrary_firewall/recipes/rabbitmq.rb
@@ -31,16 +31,6 @@ app_server_nodes.each do |app_node|
   end
 end
 
-# Get all loadbalancers
-railsweb_nodes = search(:node, "chef_environment:#{node.chef_environment} AND role:railsweb")
-
-# Allow connections from loadbalancer
-railsweb_nodes.each do |railsweb_node|
-  simple_iptables_rule "system" do
-    rule "-p tcp -s #{railsweb_node['ipaddress']} --dport 5672"
-    jump "ACCEPT"
-  end
-end
 
 # For now, allow from all addresses (cloudcontrol)
 simple_iptables_rule "system" do
