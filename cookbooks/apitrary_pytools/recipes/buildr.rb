19e618ee3f49af45626ae1d6855b8cae6c117809
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-04-05 16:29:21 +0200

    removing deployr from buildr

--- cookbooks/apitrary_pytools/recipes/buildr.rb
+++ cookbooks/apitrary_pytools/recipes/buildr.rb
@@ -10,7 +10,6 @@ include_recipe "user::default"
 include_recipe "supervisor::default"
 include_recipe "apitrary_pytools::default"
 include_recipe "apitrary_pytools::pytools"
-include_recipe "apitrary_pytools::deployr"
 
 user_account 'buildr' do
   comment   'Buildr User'
@@ -18,20 +17,6 @@ user_account 'buildr' do
   shell     '/usr/sbin/nologin'
   not_if {File.exists?("/home/buildr")}
 end
-
-#directory "/etc/deployr" do
-#  owner "root"
-#  group "root"
-#  mode 0700
-#end
-#
-#template "/etc/deployr/deployr.conf" do
-#  source "deployr.conf.erb"
-#  mode 0644
-#  owner "root"
-#  group "root"
-#  notifies :run, 'execute[supervisor_buildr_restart]', :immediately
-#end
 
 template "/etc/supervisor/conf.d/buildr.conf" do
   source "supervisor_buildr.conf.erb"
