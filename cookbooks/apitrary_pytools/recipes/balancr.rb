171b384fa746e42fe77d41f7d40002c2d8f97577
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-04-21 20:20:48 +0200

    removing old deployr, now using pydeployr from pytools

--- cookbooks/apitrary_pytools/recipes/balancr.rb
+++ cookbooks/apitrary_pytools/recipes/balancr.rb
@@ -17,12 +17,6 @@ user_account 'balancr' do
   not_if {File.exists?("/home/balancr")}
 end
 
-execute "pip-install-deployr" do
-  command "pip install git+ssh://git@github.com/apitrary/deployr.git"
-  user "root"
-  not_if {File.exists?("/usr/local/bin/deployr.py")}
-end
-
 directory "/etc/deployr" do
   owner "root"
   group "root"
